f5670733eb348bdc9d3875cf2ad4d4e957efc809
AdrianSilvadoNascimento <adrianfsf10@gmail.com>
2025-04-14 18:24:51 -0300

    implement home

---
+++ src/app/components/guests_list/guests_list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guests-list',
+  templateUrl: './guests_list.component.html',
+  styleUrls: ['./guests_list.component.scss']
+})
+export class GuestsListComponent {
+  // Lógica do componente
+} 

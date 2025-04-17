6034a7e2c02c490887ab419aac673902f168b274
Adrian Silva <75761636+AdrianSilvadoNascimento@users.noreply.github.com>
2025-04-17 08:25:22 -0300

    fix home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
@@ -11,5 +11,12 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  isMobile: boolean = window.innerWidth <= 768; // Define o limite para mobile
+
   constructor() { }
-} 
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isMobile = (event.target as Window).innerWidth <= 768;
+  }
+}
